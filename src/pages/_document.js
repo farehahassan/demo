82bff4de7bcff5f13c43a96d5ddc89840ba5dfd7
Fareha <hfariha104@gmail.com>
2023-11-02 12:50:17 +0500

    test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,6 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Navbar from '@/components/navbar'
 import Hearder from '@/components/header'
 import { Footer } from '@/components/footer'
+import { Container } from 'container'
 
 export default function Document() {
   return (
